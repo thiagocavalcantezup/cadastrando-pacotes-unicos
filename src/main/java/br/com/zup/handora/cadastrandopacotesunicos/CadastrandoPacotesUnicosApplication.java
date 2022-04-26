51fe8e29120a8af3bdf1212fb80e2d2fd5ba1848
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-04-26 00:49:05 -0300

    Initial commit

---
+++ src/main/java/br/com/zup/handora/cadastrandopacotesunicos/CadastrandoPacotesUnicosApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.handora.cadastrandopacotesunicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastrandoPacotesUnicosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CadastrandoPacotesUnicosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/zup/handora/cadastrandopacotesunicos/CadastrandoPacotesUnicosApplicationTests.java
@@ -0,0 +1,12 @@
+package br.com.zup.handora.cadastrandopacotesunicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastrandoPacotesUnicosApplicationTests {
+
+    @Test
+    void contextLoads() {}
+
+}
